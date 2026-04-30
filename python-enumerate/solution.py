f501a4b4f872512cf433d21b1d10ebc024e5e3ec
Krish Nalam <nalamkrish1@gmail.com>
2026-04-30 11:18:48 -0400

    solve: Enumerate (Python for Coding Interviews) [neetcode]

---
+++ python-enumerate/solution.py
@@ -0,0 +1,24 @@
+
+
+def get_dist_between_sevens(nums: List[int]) -> int:
+# do not modify below this line
+print(get_index_of_seven([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(get_index_of_seven([1, 2, 3, 4, 5, 6, 8, 9]))
+        if num == 7: return index
+    return -1
+    first = -1
+        if num == 7 and first == -1:
+print(get_index_of_seven([2, 4, 7, 5, 7, 8, 4, 2]))
+
+
+print(get_dist_between_sevens([1, 2, 7, 4, 5, 6, 7, 8, 9]))
+from typing import List
+
+
+def get_index_of_seven(nums: List[int]) -> int:
+    for index, num in enumerate(nums):
+            first = index
+
+    for index, num in enumerate(nums):
+        elif num == 7:
+            return index - first
